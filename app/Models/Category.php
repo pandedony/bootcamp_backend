f1c2112bbbb81a1b13fbe959e3bc5122a3340abb
pande Dony <pandedonyy@gmail.com>
2024-02-22 16:31:17 +0800

    update category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,8 +10,4 @@ class Category extends Model
 {
     use HasFactory;
     protected $fillable = ['name'];
-    public function products()
-    {
-        return $this->hasMany(Product::class);
-    }
 }
